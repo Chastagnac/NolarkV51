58315dc0942438096fcd1e2b6a1f087dd24b7e23
leochastagnac <leochastagnac@gmail.com>
2020-06-01 14:03:45 +0200

    footer changement

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>&copy;2018 Nolark - Créé par <a href="leochastagnac@gmail.com">Léo Chastagnac</a> - Page chargée le <?php
+    <p>&copy;2018 Nolark - Créé par <a href="leochastagnac@gmail.com">Chastagnac Léo</a> Benjabria Adem et Charles Valentin - Page chargée le <?php
         setlocale(LC_ALL, 'fr-FR.utf8', 'fra');
         date_default_timezone_set('Europe/Paris');
         echo strftime("%A %d %B");
